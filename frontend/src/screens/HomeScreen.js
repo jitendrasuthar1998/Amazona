c9fc1197240427783e7f018046022a3863a77652
learnwithjs <79091909+learnwithjs@users.noreply.github.com>
2021-04-06 13:56:41 +0530

    Added redux to manage product state.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,45 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios'; 
+import React, { useEffect } from 'react'; 
+import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product';
 import MessageBox from '../components/MessageBox';
 import LoadingBox from '../components/LoadingBox';
-
-
+import { listProducts } from '../actions/productActions';
 
 export default function HomeScreen() {
 
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
   
+  useEffect(()=>{
+    dispatch(listProducts());
+  }, [dispatch]);   
 
-  const [products, setProducts] = useState([]); //react hook to set state to products
-
-  
-  const [loading, setLoading] = useState(false);  //react hook to show loading 
-
-  const [error, setError] = useState(false); //react hook to show error
-
-  useEffect(() => {
-
-    //function to fetch data from backend /api/products
-
-    const fetchData = async () => {
-
-      try{
-        setLoading(true);
-      const { data } = await axios.get('/api/products'); //ajax request to get products from '/api/products.
-      setLoading(false);
-      setProducts(data);
-      }
-      catch(err){
-        setError(err.message);
-        setLoading(false); //no need to show loading anymore.
-      }
-    };
-    fetchData();
-  }, []);
-
+    
   return (
     <div>
-
       {loading ? <LoadingBox></LoadingBox>
       :
       error ? <MessageBox variant="danger">{error}</MessageBox>
