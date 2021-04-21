a188aaef0971473ea241969b26567983037394ab
learnwithjs <79091909+learnwithjs@users.noreply.github.com>
2021-04-21 13:37:34 +0530

    27. Created PlaceOrder API

--- backend/utils.js
+++ backend/utils.js
@@ -1,6 +1,6 @@
  //define some utility function like generate token
 
- import jwt from 'jsonwebtoken';
+ import jwt, { decode } from 'jsonwebtoken';
 
  export const generateToken = (user) => {
    return jwt.sign( { 
@@ -15,3 +15,20 @@
     }
     );
  };
+
+ export const isAuth = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(authorization) {
+      const token = authorization.slice(7, authorization.length); 
+      jwt.verify(token, process.env.JWT_SECTRET || 'somethingsecret', (err, decode) => {
+        if(err) {
+          res.status(401).send({message: 'Invalid Token'})
+        } else{
+          req.user = decode;
+          next();
+        }
+      })
+    } else {
+      res.status(401).send({ message: 'No token' });
+    }
+ }
