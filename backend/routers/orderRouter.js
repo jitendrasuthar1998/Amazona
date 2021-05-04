370718eb52dd3680b1ac2d6bf6b4bd4cf7df4030
learnwithjs <79091909+learnwithjs@users.noreply.github.com>
2021-05-04 23:50:41 +0530

    43. Implemented Delete order to admin

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -81,4 +81,18 @@ expressAsyncHandler(async(req, res)=>{
 })
 );
 
+orderRouter.delete('/:id',
+isAuth,
+isAdmin,
+expressAsyncHandler(async(req, res)=>{
+  const order = await Order.findById(req.params.id);
+  if(order){
+    const deleteOrder = await order.remove();
+    res.send({message: 'Order Deleted', order: deleteOrder});
+  }else{
+    res.status(404).send({message: 'Order not found'});
+  }
+})
+);
+
 export default orderRouter;
