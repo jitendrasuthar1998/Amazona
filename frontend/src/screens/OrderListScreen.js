60e8bedf56c1510e9fca4b3d902a585d7fc7d65a
learnwithjs <79091909+learnwithjs@users.noreply.github.com>
2021-05-06 17:59:36 +0530

    44. Implemented Deliver order to admin

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -60,7 +60,7 @@ export default function OrderListScreen(props) {
                 <td>{order.createdAt.substring(0,10)}</td>
                 <td>{order.totalPrice.toFixed(2)}</td>
                 <td>{order.isPaid ? order.paidAt.substring(0,10): 'No'}</td>
-                <td>{order.isDelivered ? order.deliveredAt.subString(0,10): 'No'}</td>
+                <td>{order.isDelivered ? order.deliveredAt.substring(0,10): 'No'}</td>
                 <td>
                   <button type="button" className="small" onClick={()=>{
                     props.history.push(`/order/${order._id}`);
